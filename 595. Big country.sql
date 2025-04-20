139e33abb064381191df2cc60d3caa5c03bf17ab
hanzeyyy <mhansari1107@gmail.com>
2025-04-20 20:04:28 +0530

    Q3) 595. Big country.sql

--- 595. Big country.sql
+++ 595. Big country.sql
@@ -12,7 +12,6 @@ Table: World
 name is the primary key (column with unique values) for this table.
 Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
  
-
 A country is big if:
 
 it has an area of at least three million (i.e., 3000000 km2), or
@@ -22,8 +21,6 @@ Write a solution to find the name, population, and area of the big countries.
 Return the result table in any order.
 
 The result format is in the following example.
-
- 
 
 Example 1:
 
